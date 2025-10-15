4678a387908b72c4c662b22f8611d65421d32223
rahul <nizarerahul7@gmail.com>
2025-10-15 21:36:05 +0530

    bubble sort algorithm O(n*2)

---
+++ Day-17/BubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubbleSort([77, 2, 3, 5, 1, 100, 99, 88, 23, 3]));

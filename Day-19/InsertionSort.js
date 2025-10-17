38cca9fa6cd49d3652bc96e8069ff9b5259546ea
rahul <nizarerahul7@gmail.com>
2025-10-17 22:45:50 +0530

    insertion sort approapch

--- Day-19/InsertionSort.js
+++ Day-19/InsertionSort.js
@@ -1,14 +1,12 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    let currValue = arr[i];
-    for (let j = i - 1; j >= 0; j--) {
-      if (currValue < arr[j]) {
-        arr[j + 1] = arr[j];
-      }
-      if (j == 0 && arr[j] > currValue) {
-        arr[0] = currValue;
-      }
+    let curr = arr[i];
+    let prev = i - 1;
+    while (arr[prev] > curr && prev >= 0) {
+      arr[prev + 1] = arr[prev];
+      prev--;
     }
+    arr[prev + 1] = curr;
   }
   return arr;
 }

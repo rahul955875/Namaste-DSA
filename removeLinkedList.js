5336ba2672d6124be391498185448daf36f5fb53
rahul <nizarerahul7@gmail.com>
2025-10-30 23:02:00 +0530

    remove linked list

---
+++ removeLinkedList.js
@@ -0,0 +1,38 @@
+//my approach
+
+function removeLinkedList(head, val) {
+  let prev = null;
+  let curr = head;
+
+  while (curr) {
+    if (curr.val == val && !prev) {
+      head = head.next;
+      curr = head;
+    } else if (curr.val == val) {
+      prev.next = curr.next;
+      curr = curr.next;
+    } else {
+      prev = curr;
+      curr = curr.next;
+    }
+  }
+  return head;
+}
+
+// new approach
+
+function removeLinkedList2(head, val) {
+  const newNode = { val: 0, next: null };
+  const sentinal = newNode;
+  sentinal.next = head;
+  let prev = sentinal;
+
+  while (prev.next) {
+    if (prev.next.val == val) {
+      prev.next = prev.next.next;
+    } else {
+      prev = prev.next;
+    }
+  }
+  return head;
+}

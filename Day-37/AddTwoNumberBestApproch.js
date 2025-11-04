ebc2dd31ea81e218c62e29e7b040dba294322a00
rahul <nizarerahul7@gmail.com>
2025-11-04 21:51:18 +0530

    optimize approach for add two number - linked list

---
+++ Day-37/AddTwoNumberBestApproch.js
@@ -0,0 +1,152 @@
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+var addTwoNumbers = function (l1, l2) {
+  let newList = new ListNode();
+  let saveList = newList;
+  let carry = 0;
+
+  while (l1 || l2 || carry) {
+    let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    let digit = sum % 10;
+    carry = Math.floor(sum / 10);
+
+    newList.next = new ListNode(digit);
+    newList = newList.next;
+    l1 = l1 && l1.next;
+    l2 = l2 && l2.next;
+    // console.log(carry, "carry");
+  }
+  return saveList.next;
+};
+// const l1 = {
+//   val: 2,
+//   next: {
+//     val: 4,
+//     next: {
+//       val: 3,
+//       next: null,
+//     },
+//   },
+// };
+
+// const l2 = {
+//   val: 5,
+//   next: {
+//     val: 6,
+//     next: {
+//       val: 4,
+//       next: null,
+//     },
+//   },
+// };
+
+// [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+const l2 = {
+  val: 5,
+  next: {
+    val: 6,
+    next: {
+      val: 4,
+      next: null,
+    },
+  },
+};
+const l1 = {
+  val: 1,
+  next: {
+    val: 0,
+    next: {
+      val: 0,
+      next: {
+        val: 0,
+        next: {
+          val: 0,
+          next: {
+            val: 0,
+            next: {
+              val: 0,
+              next: {
+                val: 0,
+                next: {
+                  val: 0,
+                  next: {
+                    val: 0,
+                    next: {
+                      val: 0,
+                      next: {
+                        val: 0,
+                        next: {
+                          val: 0,
+                          next: {
+                            val: 0,
+                            next: {
+                              val: 0,
+                              next: {
+                                val: 0,
+                                next: {
+                                  val: 0,
+                                  next: {
+                                    val: 0,
+                                    next: {
+                                      val: 0,
+                                      next: {
+                                        val: 0,
+                                        next: {
+                                          val: 0,
+                                          next: {
+                                            val: 0,
+                                            next: {
+                                              val: 0,
+                                              next: {
+                                                val: 0,
+                                                next: {
+                                                  val: 0,
+                                                  next: {
+                                                    val: 0,
+                                                    next: {
+                                                      val: 0,
+                                                      next: {
+                                                        val: 0,
+                                                        next: {
+                                                          val: 0,
+                                                          next: {
+                                                            val: 0,
+                                                            next: {
+                                                              val: 1,
+                                                              next: null,
+                                                            },
+                                                          },
+                                                        },
+                                                      },
+                                                    },
+                                                  },
+                                                },
+                                              },
+                                            },
+                                          },
+                                        },
+                                      },
+                                    },
+                                  },
+                                },
+                              },
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+console.dir(addTwoNumbers(l1, l2), { depth: null });

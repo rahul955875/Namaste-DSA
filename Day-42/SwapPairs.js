ea3d1e97aa7ea801a2e7c0b561213d961c3237d3
rahul <nizarerahul7@gmail.com>
2025-11-10 22:02:25 +0530

    swap pairs recursive approach

--- Day-42/SwapPairs.js
+++ Day-42/SwapPairs.js
@@ -1,4 +1,13 @@
-function swapPairs(head) {}
+function swapPairs(head) {
+  if (!head || !head.next) return head;
+  const left = head;
+  const right = head.next;
+  const rightHalf = swapPairs(right.next);
+  left.next = rightHalf;
+  right.next = left;
+
+  return right;
+}
 
 head = {
   val: 1,

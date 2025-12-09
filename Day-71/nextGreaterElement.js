34f38a371213fb200113da1e9b49efd0fafd23c1
rahul <nizarerahul7@gmail.com>
2025-12-09 19:31:14 +0530

    next greater element my approach

--- Day-71/nextGreaterElement.js
+++ Day-71/nextGreaterElement.js
@@ -6,24 +6,29 @@
 var nextGreaterElement = function (nums1, nums2) {
   const stack = [];
   const n = nums2.length;
-  const ans = Array(nums1.length).fill(-1);
+  const ans = Array(nums2.length).fill(-1);
+  const result = [];
   stack.push(nums2[n - 1]);
   for (let i = n - 2; i >= 0; i--) {
     while (stack.length) {
       const top = stack[stack.length - 1];
       if (top > nums2[i]) {
-        for (let j = 0; j < nums1.length; j++) {
-          if (nums2[i] === nums1[j]) {
-            ans[j] = nums2[top - i];
-          }
-        }
+        ans[i] = top;
+        break;
       } else {
         stack.pop();
       }
     }
-    stack.push(i);
+    stack.push(nums2[i]);
   }
-  console.log(ans);
-  return ans;
+  for (let i = 0; i < nums1.length; i++) {
+    const index = nums2.indexOf(nums1[i]);
+    if (index >= 0) {
+      result[i] = ans[index];
+    } else {
+      result[i] = -1;
+    }
+  }
+  return result;
 };
 console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]));

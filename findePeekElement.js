e6f0f972bef3de2c68b7c4e0598ba10c751f7666
rahul <nizarerahul7@gmail.com>
2025-12-17 22:19:39 +0530

    find peek element

---
+++ findePeekElement.js
@@ -0,0 +1,14 @@
+//find peek element
+function findPeekElement(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (arr[mid] > arr[mid + 1]) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left; // left == right
+}
